cd7fcb656598134e1c413ad43df6d752b572e9bf
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-07-02 12:10:42 +0200

    login logic

--- src/router.js
+++ src/router.js
@@ -1,23 +1,52 @@
 import Vue from "vue";
 import Router from "vue-router";
+import firebase from "firebase";
+
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
+      path: "*",
+      redirect: "/login"
+    },
+    {
       path: "/",
-      name: "home",
-      component: Home
+      redirect: "/login"
     },
     {
       path: "/login",
       name: "login",
       component: Login
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: Home,
+      meta: {
+        isAuth: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  let user = firebase.auth().currentUser;
+  let authRequired = to.matched.some(record => record.meta.isAuth);
+  console.log();
+
+  if (authRequired && !user) {
+    next("login");
+  } else if (to.name === "login" && user) {
+    next("home");
+  } else {
+    next();
+  }
+});
+
+export default router;
