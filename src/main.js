adf9f9b55bd69cf73ae88d96a57a3032ec0aab7f
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-07-02 08:51:19 +0200

    init project

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase'
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyACZLcOM9y301oCOlYD_F14FRRLUzF2U2w",
+  authDomain: "weekly-menu-vue.firebaseapp.com",
+  databaseURL: "https://weekly-menu-vue.firebaseio.com",
+  projectId: "weekly-menu-vue",
+  storageBucket: "",
+  messagingSenderId: "962317874675",
+  appId: "1:962317874675:web:9748fc14ec9ebeab"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form @click.prevent="login">
+      <input type="text" v-model="user">
+      <input type="email">
+      <button type="submit">Acceder</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      user: null,
+      email: null
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.user)
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
