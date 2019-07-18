a8d8f4b8a8719ab5c7517b18a06e08a5b80f0acf
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-07-18 13:38:56 +0200

    refactor components

---
+++ src/components/MealForm.vue
@@ -0,0 +1,129 @@
+<template>
+  <form>
+    <Tag
+      type="breakfast"
+      extraClass="margin-right-10"
+      @onClick="selectTag"
+      ref="breakfast"
+      :active="mealSelect && mealSelect.type === 'breakfast' ? true : null"
+    />
+    <Tag
+      type="lunch"
+      extraClass="margin-right-10"
+      @onClick="selectTag"
+      ref="lunch"
+      :active="mealSelect && mealSelect.type === 'lunch' ? true : null"
+    />
+    <Tag
+      type="dinner"
+      @onClick="selectTag"
+      ref="dinner"
+      :active="mealSelect && mealSelect.type === 'dinner' ? true : null"
+    />
+
+    <textareaField
+      label="Your meal:"
+      extraClass="margin-top-30"
+      disabled
+      ref="textareaMeal"
+      @input="e => onKeyUpMeal(e)"
+    />
+
+    <Button
+      text="Add your meal"
+      icon="drumstick-bite"
+      small
+      extraClass="margin-top-30"
+      variant="orange"
+      ref="addMealButton"
+      disabled
+      @onClick="() => addMeal()"
+    />
+
+    <p v-if="loading">Loading ...</p>
+  </form>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import Tag from "../components/Tag";
+import TextareaField from "../components/TextareaField";
+import Button from "../components/Button";
+import { updateBoard } from "../firebase/functions";
+import { eventBus } from "../event-bus.js";
+
+export default {
+  name: "meal-form",
+  components: {
+    Tag,
+    TextareaField,
+    Button
+  },
+  props: ["day", "mealSelect"],
+  data() {
+    return {
+      loading: false,
+      activeTag: null,
+      meal: null
+    };
+  },
+  mounted() {
+    if (this.mealSelect) {
+      this.meal = this.mealSelect.meal;
+      this.activeTag = this.mealSelect.type;
+      this.$refs.textareaMeal.activate();
+      this.$refs.textareaMeal.addContent(this.mealSelect.meal);
+      this.$refs.addMealButton.activeButton();
+    }
+  },
+  methods: {
+    addMeal() {
+      this.loading = true;
+      this.$store.commit("addDayMeal", { day: this.day, type: this.activeTag, meal: this.meal });
+
+      updateBoard(this.board, this.user.id).then(() => {
+        this.meal = null;
+        this.loading = false;
+        eventBus.$emit("resetField");
+        this.$refs.addMealButton.disabledButton();
+        this.$refs.textareaMeal.disable();
+        this.disableTags();
+        this.$emit("closeFormModal");
+      });
+    },
+    onKeyUpMeal(e) {
+      this.meal = e;
+      if (this.meal.length > 0) {
+        this.$refs.addMealButton.activeButton();
+      } else {
+        this.$refs.addMealButton.disabledButton();
+      }
+    },
+    selectTag(e) {
+      if (this.$refs[e].isActive) {
+        this.disableTags();
+        this.activeTag = null;
+        this.$refs.textareaMeal.disable();
+        this.$refs.addMealButton.disabledButton();
+      } else {
+        this.disableTags();
+        this.$refs[e].selectTag(true);
+        this.activeTag = e;
+        this.$refs.textareaMeal.activate();
+        if (this.length > 1) this.$refs.addMealButton.activeButton();
+      }
+    },
+    disableTags() {
+      this.$refs.breakfast.selectTag(false);
+      this.$refs.lunch.selectTag(false);
+      this.$refs.dinner.selectTag(false);
+    }
+  },
+  computed: {
+    ...mapState(["board", "user"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
