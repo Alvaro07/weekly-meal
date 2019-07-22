cec44823ae721b15b498a29e893963e2afc3acbb
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-07-22 13:13:11 +0200

    'deploy'

---
+++ dist/precache-manifest.6b86090bffdaca342b6ad94d967c82ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weekly-meal/robots.txt"
+  },
+  {
+    "revision": "b7fa1cdac4ffec7ebf8b",
+    "url": "/weekly-meal/js/chunk-vendors.07d8db3e.js"
+  },
+  {
+    "revision": "5b9721f5ab4ce9b15f5d",
+    "url": "/weekly-meal/js/app.f29f9f10.js"
+  },
+  {
+    "revision": "887440b3b5cdea57bfbc6717aa29ad3b",
+    "url": "/weekly-meal/index.html"
+  },
+  {
+    "revision": "5b9721f5ab4ce9b15f5d",
+    "url": "/weekly-meal/css/app.2b45ba88.css"
+  },
+  {
+    "revision": "c36480a3e922af22d26eeeb2c728317d",
+    "url": "/weekly-meal/404.html"
+  }
+];
